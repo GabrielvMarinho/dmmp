fc9a62b82718b3f5bb42f54007a72e50ca700135
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-12-09 13:47:26 -0300

    Add type hinting

--- src/dmmp/mapper.py
+++ src/dmmp/mapper.py
@@ -2,11 +2,11 @@ import os
 from tqdm import tqdm
 
 class Mapper():
-   def __init__(self, save_path, folders_to_ignore):
+   def __init__(self, save_path: str, folders_to_ignore: list[str]):
       self.folders_to_ignore = {item for item in folders_to_ignore}
       self.save_path = save_path
       self.progress_bar = tqdm(total=100)
-      self.sum_folder = 0
+      self.folders_sum = 0
       self.temp_mapping = {}
    
    def __call__(self, dir_to_map):
@@ -17,8 +17,12 @@ class Mapper():
          load_bar_end = (index+1)/len(dir_to_map)*100
          self.read_dir(path, nested_folders, load_bar=True, load_bar_start=load_bar_start, load_bar_end=load_bar_end)
       self.write_map()
+      self.progress_bar.n = 100
+      self.progress_bar.refresh()
+      self.progress_bar.close()
+      print(f"{self.folders_sum} folders were scanned.")
 
-   def read_dir(self, dir_, max_rec, current_rec=0, load_bar=False, load_bar_start=0, load_bar_end=100):
+   def read_dir(self, dir_: str, max_rec: int, current_rec: int=0, load_bar: bool=False, load_bar_start: int=0, load_bar_end: int=100):
       if dir_.endswith("desc.dmmp"):
          with open(dir_, "r", encoding="utf-8") as f:
             id = f.readline().strip()
@@ -35,13 +39,9 @@ class Mapper():
                "origin":"/".join(dir_.split("\\")[:-1])
             }
       
-      if not os.path.isdir(dir_):
+      if current_rec>max_rec or not os.path.isdir(dir_):
          return
       
-      if current_rec>max_rec:
-         return
-      
-      global sum
       folders = os.listdir(dir_)
       if load_bar:
          for index, folder in enumerate(folders):
@@ -53,12 +53,12 @@ class Mapper():
             self.progress_bar.set_postfix(task=folder)
             if folder not in self.folders_to_ignore:      
                self.read_dir(fr"{dir_}\{folder}", max_rec=max_rec, current_rec=current_rec+1)
-               self.sum_folder = self.sum_folder+1
+               self.folders_sum = self.folders_sum+1
       else:
          for folder in folders:
             if folder not in self.folders_to_ignore:      
                self.read_dir(fr"{dir_}\{folder}", max_rec=max_rec, current_rec=current_rec+1)
-               self.sum_folder = self.sum_folder+1
+               self.folders_sum = self.folders_sum+1
 
    def write_map(self):
       for id, object in self.temp_mapping.items():
