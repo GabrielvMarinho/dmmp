cee0d86bdf91635ba4e36fc1d4c2a2090df2f2b0
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-12-09 11:16:50 -0300

    Add minimal args support

--- src/dmmp/main.py
+++ src/dmmp/main.py
@@ -1,8 +1,19 @@
 import os
 from dotenv import load_dotenv
-load_dotenv()
+import argparse
 
-path = os.getenv("PATH_TO_MAP")
+parser = argparse.ArgumentParser(
+   prog="dmm",
+   description="directory metadata mapper"
+)
+parser.add_argument('-d', '--dir-to-map', required=True, help="directory to scan + number of nested folders to iterate", nargs=2, action="append")
+parser.add_argument('-i', '--folders-ignore', required=False, help="list of folders to ignore", nargs="+")
+
+
+load_dotenv()
+args = parser.parse_args()
+
+
 map = os.getenv("PATH_TO_SAVE_MAP")
 
 folders_to_ignore = {item for item in os.getenv("FOLDERS_TO_IGNORE").split(",")}
@@ -10,7 +21,7 @@ sum_ = {}
 
 structure = {}
 
-def read_dir(dir_, max_rec, current_rec=0, load_bar=False):
+def read_dir(dir_, max_rec, current_rec=0, load_bar=False, load_bar_start=0, load_bar_end=100):
 
    if dir_.endswith("desc.dmmp"):
       with open(dir_, "r", encoding="utf-8") as f:
@@ -37,8 +48,11 @@ def read_dir(dir_, max_rec, current_rec=0, load_bar=False):
    global sum
    folders = os.listdir(dir_)
    if load_bar:
-      for index, folder  in enumerate(folders):
-         print(f"load bar em {round(index/len(folders)*100, 2)}% -> {folder}")
+      for index, folder in enumerate(folders):
+         percentage = index/len(folders)
+         diff = load_bar_end-load_bar_start
+         relative_percentage = (percentage*diff)+load_bar_start
+         print(f"----- {relative_percentage:.2f}% -> {folder}")
          if folder not in folders_to_ignore:      
             read_dir(fr"{dir_}\{folder}", max_rec=max_rec, current_rec=current_rec+1)
             sum_[folder] = (sum_.get(folder) or 0) + 1
@@ -72,6 +86,16 @@ relationships: {
  
           
 if __name__ == "__main__":
-   read_dir(path, 1, load_bar=True)
+   
+   print(args.dir_to_map)
+   print(args.folders_ignore)
+
+   for index, array in enumerate(args.dir_to_map):
+      path = array[0]
+      nested_folders = int(array[1])
+      load_bar_start = index/len(args.dir_to_map)*100
+      load_bar_end = (index+1)/len(args.dir_to_map)*100
+      read_dir(path, nested_folders, load_bar=True, load_bar_start=load_bar_start, load_bar_end=load_bar_end)
+   print("----- 100% -----")
    print(structure)
    write_map()
