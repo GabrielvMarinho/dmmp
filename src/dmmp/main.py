a43841759ddde22c6a7f0984ca6073e620a8ce26
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-12-09 14:15:04 -0300

    Update type hinting and break methods into more modular pieces

--- src/dmmp/main.py
+++ src/dmmp/main.py
@@ -5,16 +5,17 @@ from mapper import Mapper
 load_dotenv()
 
 parser = argparse.ArgumentParser(
-   prog="dmm",
-   description="directory metadata mapper"
+   prog="DMM",
+   description="Directory Metadata Mapper"
 )
-parser.add_argument('-d', '--dir-to-map', required=True, help="directory to scan + number of nested folders to iterate", nargs=2, action="append")
-parser.add_argument('-i', '--folders-ignore', required=False, help="list of folders to ignore", nargs="+")
-parser.add_argument('-s', '--save-path', required=True, help="path to save output")
+parser.add_argument('-d', '--dir-to-map', required=True, help="Directory to scan + number of nested folders to iterate", nargs=2, action="append")
+parser.add_argument('-i', '--folders-ignore', required=False, help="List of folders to ignore", nargs="+")
+parser.add_argument('-s', '--save-path', required=True, help="Path to save output")
+parser.add_argument('-m', '--metadata-file-name', required=True, help="File name containing the directory metadata (no extension)")
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
-   mapper = Mapper(args.save_path, args.folders_ignore)
+   mapper = Mapper(args.save_path, args.folders_ignore, args.metadata_file_name)
    mapper(args.dir_to_map)
    
--- src/dmmp/mapper.py
+++ src/dmmp/mapper.py
@@ -2,14 +2,15 @@ import os
 from tqdm import tqdm
 
 class Mapper():
-   def __init__(self, save_path: str, folders_to_ignore: list[str]):
+   def __init__(self, save_path: str, folders_to_ignore: list[str], metadata_file_name: str="desc"):
       self.folders_to_ignore = {item for item in folders_to_ignore}
       self.save_path = save_path
       self.progress_bar = tqdm(total=100)
       self.folders_sum = 0
+      self.metadata_file_name = metadata_file_name
       self.temp_mapping = {}
    
-   def __call__(self, dir_to_map):
+   def __call__(self, dir_to_map: str):
       for index, array in enumerate(dir_to_map):
          path = array[0]
          nested_folders = int(array[1])
@@ -17,14 +18,35 @@ class Mapper():
          load_bar_end = (index+1)/len(dir_to_map)*100
          self.read_dir(path, nested_folders, load_bar=True, load_bar_start=load_bar_start, load_bar_end=load_bar_end)
       self.write_map()
-      self.progress_bar.n = 100
-      self.progress_bar.refresh()
-      self.progress_bar.close()
+      self.update_progress_bar(percentage=100, post_fix="Finished", close=True)
       print(f"{self.folders_sum} folders were scanned.")
 
-   def read_dir(self, dir_: str, max_rec: int, current_rec: int=0, load_bar: bool=False, load_bar_start: int=0, load_bar_end: int=100):
-      if dir_.endswith("desc.dmmp"):
-         with open(dir_, "r", encoding="utf-8") as f:
+   def update_progress_bar(self, percentage: float, post_fix: str, close: bool=False):
+      self.progress_bar.n = float(f"{percentage:.2f}")
+      self.progress_bar.refresh()
+      self.progress_bar.set_postfix_str(post_fix)
+      if close:
+         self.progress_bar.close()
+
+   def read_dir(self, directory: str, max_rec: int, current_rec: int=0, load_bar: bool=False, load_bar_start: int=0, load_bar_end: int=100):
+      if directory.endswith(f"{self.metadata_file_name}.dmmp"):
+         self.get_desc_data(directory)
+      if current_rec>max_rec or not os.path.isdir(directory):
+         return
+      folders = os.listdir(directory)
+      
+      for index, folder in enumerate(folders):
+         if folder not in self.folders_to_ignore:      
+            self.read_dir(fr"{directory}\{folder}", max_rec=max_rec, current_rec=current_rec+1)
+            self.folders_sum = self.folders_sum+1
+         if load_bar:
+            percentage = index/len(folders)
+            diff = load_bar_end-load_bar_start
+            relative_percentage = (percentage*diff)+load_bar_start
+            self.update_progress_bar(percentage=relative_percentage, post_fix=folder)
+
+   def get_desc_data(self, directory: str):
+      with open(directory, "r", encoding="utf-8") as f:
             id = f.readline().strip()
             name = f.readline().strip()
             desc = f.readline().strip()
@@ -36,32 +58,11 @@ class Mapper():
                "folder":type_,
                "type":type_,
                "relationships":relationships,
-               "origin":"/".join(dir_.split("\\")[:-1])
+               "origin":"/".join(directory.split("\\")[:-1])
             }
-      
-      if current_rec>max_rec or not os.path.isdir(dir_):
-         return
-      
-      folders = os.listdir(dir_)
-      if load_bar:
-         for index, folder in enumerate(folders):
-            percentage = index/len(folders)
-            diff = load_bar_end-load_bar_start
-            relative_percentage = (percentage*diff)+load_bar_start
-            self.progress_bar.n = float(f"{relative_percentage:.2f}")
-            self.progress_bar.refresh()
-            self.progress_bar.set_postfix(task=folder)
-            if folder not in self.folders_to_ignore:      
-               self.read_dir(fr"{dir_}\{folder}", max_rec=max_rec, current_rec=current_rec+1)
-               self.folders_sum = self.folders_sum+1
-      else:
-         for folder in folders:
-            if folder not in self.folders_to_ignore:      
-               self.read_dir(fr"{dir_}\{folder}", max_rec=max_rec, current_rec=current_rec+1)
-               self.folders_sum = self.folders_sum+1
 
    def write_map(self):
-      for id, object in self.temp_mapping.items():
+      for _, object in self.temp_mapping.items():
          path = self.save_path+"/"+object.get("folder")
          os.makedirs(path, exist_ok=True)
          with open(f"{path}/{object.get("name")}.md", "w", encoding="utf-8") as f1:
