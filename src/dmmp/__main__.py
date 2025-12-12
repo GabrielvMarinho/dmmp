b5be12ba9b14631af61560697eb333de6f2de475
GabrielvMarinho <147559808+GabrielvMarinho@users.noreply.github.com>
2025-12-12 14:35:25 -0300

    Add suport for .toml config files

--- src/dmmp/__main__.py
+++ src/dmmp/__main__.py
@@ -1,21 +1,41 @@
 from dotenv import load_dotenv
 import argparse
+import os
 from .mapper import Mapper
+from .args import get_args, validate_args, write_args_to_toml
+
 load_dotenv()
 
 parser = argparse.ArgumentParser(
    prog="DMM",
    description="Directory Metadata Mapper"
 )
-parser.add_argument('-d', '--dir-to-map', required=True, help="Directory to scan + number of nested folders to iterate", nargs=2, action="append")
+parser.add_argument('-c', '--config-file', default="config.toml", required=False, help=".toml file to get configuration from ") 
+parser.add_argument('-d', '--dir-to-map', required=False, help="Directory to scan + number of nested folders to iterate", nargs=2, action="append")
 parser.add_argument('-i', '--folders-ignore', required=False, help="List of folders to ignore", nargs="+")
-parser.add_argument('-s', '--save-path', required=True, help="Path to save output")
-parser.add_argument('-n', '--name-output', default="dmmp-output", required=False, help="Name of the output directory")
-parser.add_argument('-m', '--metadata-file-names', required=False, default=["desc"], nargs="+", help="File names containing the directory metadata (no extension)")
+parser.add_argument('-s', '--save-path', required=False, help="Path to save output")
+parser.add_argument('-n', '--name-output', required=False, help="Name of the output directory")
+parser.add_argument('-m', '--metadata-file-names', required=False, nargs="+", help="File names containing the directory metadata (no extension)")
+
+required_args = [
+   "dir_to_map",
+   "folders_ignore",
+   "save_path",
+   "name_output",
+   "metadata_file_names",
+    ]
 
 args = parser.parse_args()
 
 if __name__ == "__main__":
-   mapper = Mapper(args.save_path, args.name_output, args.folders_ignore, args.metadata_file_names)
-   mapper(args.dir_to_map)
-     
+   formatted_args = get_args(args)
+   
+   validate_args(formatted_args, required_args=required_args)
+
+   mapper = Mapper(formatted_args.get("save_path"),
+                  formatted_args.get("name_output"), 
+                  formatted_args.get("folders_ignore"), 
+                  formatted_args.get("metadata_file_names"))
+   mapper(formatted_args.get("dir_to_map"))
+   
+   write_args_to_toml(formatted_args, formatted_args["config_file"])
---
+++ src/dmmp/args.py
@@ -0,0 +1,56 @@
+import tomllib
+import tomli_w
+import os
+from .exceptions import MissingArgumentException
+
+
+def _get_toml_args(path: str):
+   try:
+      print(path)
+      with open(path, "rb") as f:
+         data = tomllib.load(f)
+         print(data)
+         return data
+      
+   except Exception as e:
+      print(e)
+      return {}
+
+
+def get_args(args):
+   toml_dict = _get_toml_args(args.config_file)
+
+   cli_dict = {
+      k:v for k, v in {
+      "config_file":args.config_file,
+      "dir_to_map":args.dir_to_map,
+      "folders_ignore":args.folders_ignore,
+      "save_path":args.save_path,
+      "name_output":args.name_output,
+      "metadata_file_names":args.metadata_file_names,
+      }.items() if v !=None
+   }
+   merged_dict = toml_dict | cli_dict
+
+   # the reason for this is, cli arguments should be priority, but only if its not a default one, 
+   # default ones will ONLY be written if the toml ones are empty
+   merged_dict["folders_ignore"] = [] if not merged_dict.get("folders_ignore") else merged_dict["folders_ignore"] 
+   merged_dict["save_path"] = os.getcwd() if not merged_dict.get("save_path") else merged_dict["save_path"] 
+   merged_dict["name_output"] = "dmmp-output" if not merged_dict.get("name_output") else merged_dict["name_output"] 
+   merged_dict["config_file"] = "config.toml" if not merged_dict.get("config_file") else merged_dict["config_file"] 
+   merged_dict["metadata_file_names"] = ["*"] if not merged_dict.get("metadata_file_names") else merged_dict["metadata_file_names"] 
+
+   return merged_dict
+
+
+def validate_args(args: dict, required_args: list[str]):
+   for arg in required_args:
+      if arg not in args.keys():
+         raise MissingArgumentException(arg)
+    
+
+def write_args_to_toml(args: dict, config_file_name: str):
+   with open(os.path.join(os.getcwd(), config_file_name), "wb") as f:
+      print(args)
+      tomli_w.dump(args, f)
+ 
